1b97590785612292fb430169a5e8dc11f3620fe2
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-04-22 14:54:33 -0700

    solved weird grid issues

--- src/pages/Production.js
+++ src/pages/Production.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Tooltip from '@material-ui/core/Tooltip';
 import Hero from '../components/Hero';
 import Footer from '../components/Footer';
 import production1 from '../assets/images/production-filler-1.png';
@@ -19,10 +20,13 @@ const useStyles = makeStyles((theme) => ({
     bigImage: {
         boxShadow: '5px 10px 10px',
         height: '70vh',
+        marginBottom: '2vh'
     },
     smallImage: {
         boxShadow: '5px 10px 10px',
-        height: '10vh'
+        height: '30vh',
+        marginBottom: '1vh',
+        marginLeft: '1vw'
     }
 }));
 
@@ -31,21 +35,95 @@ const Production = () => {
     const classes = useStyles();
     return (
         <div className={classes.root}>
-            <Grid container spacing={3}>
-                <Grid item xs={12} id="top">
-                    <Hero />
-                </Grid>
+            <Grid item xs={12} id="top">
+                <Hero />
             </Grid>
+
             <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <Paper className={classes.paper}>
                         <h1>Lime and Cheese Salad</h1>
+                        <p>The Papua New Ginuea Playhouse, 1955</p>
                     </Paper>
                 </Grid>
             </Grid>
+
             <Grid container spacing={3}>
-                <Grid item xs={5}>
-                    <img src={`${production1}`} alt="Lime Cheese Monstrosity" className={classes.bigImage}></img>
+                <Grid item xs={6}>
+                    <Tooltip title="Lime Cheese Monstrocity">
+                        <img src={`${production1}`} alt="Lime Cheese Monstrosity" className={classes.bigImage}></img>
+                    </Tooltip>
+                </Grid>
+                <Grid item xs={6}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={6}>
+                            <Tooltip title="Lime Cheese Monstrocity">
+                                <img src={`${production1}`} alt="Lime Cheese Monstrosity" className={classes.smallImage}></img>
+                            </Tooltip>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Tooltip title="Lime Cheese Monstrocity">
+                                <img src={`${production1}`} alt="Lime Cheese Monstrosity" className={classes.smallImage}></img>
+                            </Tooltip>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={3}>
+                        <Grid item xs={6}>
+                            <Tooltip title="Lime Cheese Monstrocity">
+                                <img src={`${production1}`} alt="Lime Cheese Monstrosity" className={classes.smallImage}></img>
+                            </Tooltip>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Tooltip title="Lime Cheese Monstrocity">
+                                <img src={`${production1}`} alt="Lime Cheese Monstrosity" className={classes.smallImage}></img>
+                            </Tooltip>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+
+
+
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>
+                        <h3>Sometimes, these old recipes start out innocent enough, and then shock you with their horrifying contents. Not this one. Nope, it's awful from the start. Like, lime Jell-O? Are you kidding? And wtf are those weird red meat chunks in the center? Also, I don't see any cheese, so I assume that's the weird white globs in the bottom part of the Jell-O monstrocity. And while we're on the subject, what KIND of cheese are we talking here? 'Cheese' is a bit vague for my comfort levels, to be honest with you. But they do provide the onlooker with a nice limp lettuce leaf for their troubles, so I guess props to them for that.</h3>
+                        <p>Production photos by Satan (praise be unto him)</p>
+                    </Paper>
+                </Grid>
+            </Grid>
+
+            <Grid container spacing={3}>
+                <Grid item xs={6}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={6}>
+                            <Tooltip title="Lime Cheese Monstrocity">
+                                <img src={`${production1}`} alt="Lime Cheese Monstrosity" className={classes.smallImage}></img>
+                            </Tooltip>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Tooltip title="Lime Cheese Monstrocity">
+                                <img src={`${production1}`} alt="Lime Cheese Monstrosity" className={classes.smallImage}></img>
+                            </Tooltip>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={3}>
+                        <Grid item xs={6}>
+                            <Tooltip title="Lime Cheese Monstrocity">
+                                <img src={`${production1}`} alt="Lime Cheese Monstrosity" className={classes.smallImage}></img>
+                            </Tooltip>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Tooltip title="Lime Cheese Monstrocity">
+                                <img src={`${production1}`} alt="Lime Cheese Monstrosity" className={classes.smallImage}></img>
+                            </Tooltip>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                    <Tooltip title="Lime Cheese Monstrocity">
+                        <img src={`${production1}`} alt="Lime Cheese Monstrosity" className={classes.bigImage}></img>
+                    </Tooltip>
                 </Grid>
             </Grid>
 
